560a34c8f9381fa29b2abcef76e822e6604ccb43
nishfath <81247855+nishfath@users.noreply.github.com>
2025-05-01 13:59:16 -0400

    Fixing src/main/java/org/joychou/util/WebUtils.java for finding 67

--- src/main/java/org/joychou/util/WebUtils.java
+++ src/main/java/org/joychou/util/WebUtils.java
@@ -35,12 +35,21 @@ public class WebUtils {
     }
 
 
-    public static String getFileExtension(String fullName) {
-        Preconditions.checkNotNull(fullName);
-        String fileName = (new File(fullName)).getName();
-        int dotIndex = fileName.lastIndexOf('.');
-        return dotIndex == -1 ? "" : fileName.substring(dotIndex + 1);
+public static String getFileExtension(String fullName) {
+    Preconditions.checkNotNull(fullName);
+    
+    // Don't create File object directly from user input
+    // Instead, extract the extension safely using FilenameUtils
+    try {
+        URI uri = new URI(fullName);
+        String path = uri.getPath();
+        return FilenameUtils.getExtension(path);
+    } catch (URISyntaxException e) {
+        // If not a valid URI, try to extract extension safely
+        return FilenameUtils.getExtension(fullName);
     }
+}
+
 
 
     public static String getNameWithoutExtension(String file) {
