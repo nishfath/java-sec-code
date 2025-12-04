37e6cc06d1cd53f8ff014365af786712dc423dd3
Qwiet AI <noreply@shiftleft.io>
2025-12-04 18:43:30 -0500

    Fixing src/main/java/org/joychou/util/WebUtils.java for finding 182

--- src/main/java/org/joychou/util/WebUtils.java
+++ src/main/java/org/joychou/util/WebUtils.java
@@ -12,17 +12,19 @@ import org.springframework.web.util.HtmlUtils;
 public class WebUtils {
 
     // Get request body.
-    public static String getRequestBody(HttpServletRequest request) throws IOException {
-        InputStream in = request.getInputStream();
-        return convertStreamToString(in);
-    }
+public static String getRequestBody(HttpServletRequest request) throws IOException {
+    InputStream in = request.getInputStream();
+    return convertStreamToString(in);
+}
+
 
 
     // https://stackoverflow.com/questions/309424/how-do-i-read-convert-an-inputstream-into-a-string-in-java
-    public static String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
+public static String convertStreamToString(java.io.InputStream is) {
+    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+}
+
 
     public static String getCookieValueByName(HttpServletRequest request, String cookieName) {
         Cookie cookie = org.springframework.web.util.WebUtils.getCookie(request, cookieName);
